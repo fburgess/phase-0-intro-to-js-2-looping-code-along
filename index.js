d9300abc3bacd09d794389a53f9800d42defe900
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-01-21 18:09:48 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(list, description) {
+    let messages = [];
+
+    list.forEach((item) => {
+        messages.push(`Thank you, ${item}, for the wonderful ${description} gift!`)
+    });
+
+    return messages;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
